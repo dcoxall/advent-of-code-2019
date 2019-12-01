d5f28c748e92fa98ea3661a0872cb825c1c2c745
Darren Coxall <darren.coxall@boardintelligence.com>
2019-12-01 22:05:15 +0000

    Condense the nim code for Day 01

--- 01/nim/part01.nim
+++ 01/nim/part01.nim
@@ -1,23 +1,14 @@
 # Day 1: The Tyranny of the Rocket Equation
 # https://adventofcode.com/2019/day/1
 
-# import strutils so we can use parseInt
 import strutils
 
-# load the input
 let input = open("./inputs/01.txt")
-
-# initialize our fuel
 var fuel = 0
 
-# iterate over each line of input
 for line in input.lines:
-  # add the new fuel calculation to running fuel total
-  # we need to convert the line into an integer using parseInt
   fuel += int((parseInt(line) / 3) - 2)
 
-# close the input file as we don't need it anymore
 input.close()
 
-# output the fuel
 echo fuel
