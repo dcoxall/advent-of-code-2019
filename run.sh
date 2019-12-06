6b657d65488783dc65c71d9eccf51879f03fdbe5
Darren Coxall <darren.coxall@boardintelligence.com>
2019-12-06 19:29:52 +0000

    Add release options
    I enjoy comparing the speed of the various languages and so I decided to
    make it easier for me to build all the binaries. You know, for fair
    testing.

--- run.sh
+++ run.sh
@@ -6,4 +6,12 @@ case "$1" in
   ruby)   ruby "${DAY}/ruby/${PART}.rb";;
   go)     go run "${DAY}/go/${PART}.go";;
   erlang) escript "${DAY}/erlang/${PART}.erl";;
+
+  # Options just to compile for testing
+  nimrelease)
+    nim compile -f -d:release --checks:off --assertions:off --opt:speed \
+      -o:"bin/release/nim-${DAY}-${PART}" "${DAY}/nim/${PART}.nim";;
+
+  gorelease)
+    go build -o "bin/release/go-${DAY}-${PART}" "${DAY}/go/${PART}.go";;
 esac
